6c868d1135c63e19a80dbaaff69f658b4928b117
Jorgen Skogmo <js@dearstudio.dk>
2024-07-04 09:46:55 +0200

    ping script

---
+++ scripts/ping.js
@@ -0,0 +1,93 @@
+import child_process from 'child_process';
+import util from 'util';
+import { readFileSync, writeFileSync, appendFileSync } from 'fs';
+import { hostname } from 'os';
+
+const HOSTS = [
+  'ds-opc.netbird.cloud',
+  'google.com',
+  'dearstudio.dk'
+]
+
+const LOGFILE = "ping-data.json"
+const BIGLOGFILE = "ping-data-log.txt"
+const PINGCOUNT = 1
+const PINGTIMEOUT = 2 // > 0.1
+const LOOPDELAY = 1000 * 60; // minute
+
+const exec = util.promisify(child_process.exec);
+
+const logdata = {}
+
+exec(`touch ${LOGFILE}`);
+exec(`touch ${BIGLOGFILE}`);
+
+
+async function is_online(hostname, err = '', out = '') {
+  // console.log('stderr:', err);
+  // console.log('stdout:', out);
+  if( err !== '' ){
+    // console.log('is_online:', false);
+    update_log(hostname, '-', 'offline')
+  }else{
+    // console.log('is_online:', true);
+    
+    const lines = out.split('\n');
+    // console.log('lines:', lines);
+    const ip = lines[0].split("PING")[1].split(":")[0].split("(")[1].split(")")[0].trim()
+    // console.log('ip:', ip);
+    lines.forEach(line => {
+      // round-trip min/avg/max/stddev = 228.148/228.148/228.148/0.000 ms
+      if( line.startsWith("round-trip")){
+        const stats = line.split("=")[1].trim()
+        const avg = stats.split("/")[1].trim()
+        // console.log('stats:', stats);
+        // console.log('avg:', avg);
+        update_log(hostname, ip, avg)
+      }
+    })
+  }
+}
+
+async function update_log(host, ip, avg){
+  const time = new Date().toISOString()
+  logdata[host] = {
+    time,
+    ip,
+    response: avg
+  }
+  appendFileSync(BIGLOGFILE, `${time}; ${host}; ${ip}; ${avg}\n`)
+}
+
+async function ping(hostname) {
+  try {
+    const {stdout, stderr} = await exec(`ping -c ${PINGCOUNT} -i ${PINGTIMEOUT} ${hostname}`);
+    is_online(hostname, stderr, stdout)
+  } catch (err) {
+    is_online(hostname, err, '')
+  }
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+async function tping(hostname){
+  console.log("tping", hostname)
+  update_log(hostname, 'test', '')
+  await sleep(LOOPDELAY);
+}
+
+
+
+async function main(){
+  while(true){
+    for(let i = 0; i<HOSTS.length; i++){
+      // await tping(HOSTS[i])
+      await ping(HOSTS[i])
+    }
+    console.log("logdata", logdata)
+    writeFileSync(LOGFILE, JSON.stringify(logdata, null, '  '))
+    await sleep(1000);
+  }
+}
+
+main()
